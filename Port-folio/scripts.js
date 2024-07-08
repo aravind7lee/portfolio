6a149fe862bbe352a6e909601572de2f351dd664
aravind7lee <98862841+aravind7lee@users.noreply.github.com>
2024-07-08 18:54:59 +0530

    Add files via upload

---
+++ Port-folio/scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+
+    hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+
+    document.querySelectorAll('.nav-menu a').forEach(link => {
+        link.addEventListener('click', () => {
+            navMenu.classList.remove('active');
+        });
+    });
+});
